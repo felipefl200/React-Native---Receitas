354ed036b7d42fccd8b1679ff451575aefb7f42e
felipefl200 <felipefl200@gmail.com>
2023-04-05 15:32:44 -0300

    First commit React Native

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Tela inicial</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "red",
+    height: 100,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
-});
+  title: {   
+    fontSize: 24,
+    
+
+  }
+})
