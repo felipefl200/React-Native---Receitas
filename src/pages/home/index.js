33510492e8f9d8531e8affbc2c133ada45295548
felipefl200 <felipefl200@gmail.com>
2023-04-06 14:41:58 -0300

    Add server, axios and Logo component

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,16 +1,75 @@
-import { View, Text, StyleSheet } from "react-native"
+import { useState } from "react"
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native"
+
+import { Ionicons } from "@expo/vector-icons"
+
+import { Logo } from "../../components/logo"
 
 export function Home() {
+  const [inputValue, setInputValue] = useState("")
+
+  function handleSearch() {
+    console.log("clicou")
+  }
   return (
-    <View style={styles.container}>
-      <Text>Pagina Home X</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Logo />
+      <Text style={styles.title}>Encontre a receita</Text>
+      <Text style={styles.title}>que combina com você</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          onChangeText={(text) => setInputValue(text)}
+          value={inputValue}
+          style={styles.input}
+          placeholder="Digite o nome da comida..."
+        />
+        <TouchableOpacity onPress={handleSearch}>
+          <Ionicons name="search" size={28} color="#4cbe6c" />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "red",
+    flex: 1,
+    backgroundColor: "#f3f9ff",
+    paddingTop: 36,
+    paddingStart: 14,
+    paddingEnd: 14,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: "bold",
+    color: "#0e0e0e",
+  },
+  form: {
+    backgroundColor: "#fff",
+    width: "100%",
+    borderRadius: 8,
+    marginTop: 16,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: "#ececec",
+    paddingLeft: 8,
+    paddingRight: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  input: {
+    width: "90%",
+    maxWidth: "90%",
+    height: 54,
   },
 })
 
